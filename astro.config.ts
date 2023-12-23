d7e1188ac41ec5e25fc3bffa750c1f5c44d974d0
kittichanr <petchkubbb@gmail.com>
2023-12-23 11:14:41 +0700

    Add Layout & Homepage

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	// site: "",
+	site: "https://astro-cactus.chriswilliams.dev/", // FIXME: when deploy
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { elementHasClass, toggleClass, rootHasDarkClass } from "./domElement";
