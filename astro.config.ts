03c210900851cafd5cf7f9ffc4c30f6a6b92c822
kittichanr <petchkubbb@gmail.com>
2023-12-23 08:12:02 +0700

    Setup tailwindcss

---
+++ astro.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "astro/config"
+import fs from "fs"
+
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+
+// https://astro.build/config
+export default defineConfig({
+  // site: "",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  vite: {
+    plugins: [rawFonts([".ttf"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+})
+
+function rawFonts(ext: Array<string>) {
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id)
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null,
+        }
+      }
+    },
+  }
+}
