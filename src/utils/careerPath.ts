f267ffb24719bc5c783e6f63d612aab2b033ac28
kittichanr <petchkubbb@gmail.com>
2024-06-30 07:03:26 +0700

    fix: font size in project

--- src/utils/careerPath.ts
+++ src/utils/careerPath.ts
@@ -6,12 +6,12 @@ export const careerPath = [
         description: `
         <p>
             <ul class="list-inside list-disc -indent-5 ml-6">
-                <li class="leading-6">Design solution & implement change language app on aws s3</li>
-                <li class="leading-6">Implement & Provide API to Client in microservice (gPRC), api-gateway (Graphql)</li>
-                <li class="leading-6">Maintain project upgrade libs for prevent vulnerability</li>
-                <li class="leading-6">Monitoring issue with Grafana & Kibana</li>
-                <li class="leading-6">Setup tracing in Restaurant Solution Service to use traceid investigate error request in WMA App</li>
-                <li class="leading-6">Init automate fix version jira for QA team</li>
+                <li class="leading-8">Design solution & implement change language app on aws s3</li>
+                <li class="leading-8">Implement & Provide API to Client in microservice (gPRC), api-gateway (Graphql)</li>
+                <li class="leading-8">Maintain project upgrade libs for prevent vulnerability</li>
+                <li class="leading-8">Monitoring issue with Grafana & Kibana</li>
+                <li class="leading-8">Setup tracing in Restaurant Solution Service to use traceid investigate error request in WMA App</li>
+                <li class="leading-8">Init automate fix version jira for QA team</li>
             </ul>
         </p>
         `,
@@ -23,15 +23,15 @@ export const careerPath = [
         description: `
         <p>
             <ul class="list-inside list-disc -indent-5 ml-6">
-                <li class="leading-6">Design and developed 3 mobile applications for merchant users using React Native Framework.</li>
-                <li class="leading-6">Design and developed order receipt in applications on Sunmi devices.</i>
-                <li class="leading-6">Collaborated with backend for design and integration API with Graphql, Rest API</li>
-                <li class="leading-6">Design CI/CD workflows on mobiles project.</li>
-                <li class="leading-6">Design and developed webview to connect on applications in some features, using React Framework.</li>
-                <li class="leading-6">Developed internal library ex: ui, icons, hooks, utils.</li>
-                <li class="leading-6">Developed storybook for see UI component to used in some kind of projects.</li>
-                <li class="leading-6">Developed internal website such as a admin website using React.</li>
-                <li class="leading-6">All of projects using Typescript language.</li>
+                <li class="leading-8">Design and developed 3 mobile applications for merchant users using React Native Framework.</li>
+                <li class="leading-8">Design and developed order receipt in applications on Sunmi devices.</i>
+                <li class="leading-8">Collaborated with backend for design and integration API with Graphql, Rest API</li>
+                <li class="leading-8">Design CI/CD workflows on mobiles project.</li>
+                <li class="leading-8">Design and developed webview to connect on applications in some features, using React Framework.</li>
+                <li class="leading-8">Developed internal library ex: ui, icons, hooks, utils.</li>
+                <li class="leading-8">Developed storybook for see UI component to used in some kind of projects.</li>
+                <li class="leading-8">Developed internal website such as a admin website using React.</li>
+                <li class="leading-8">All of projects using Typescript language.</li>
             </ul>
         </p>
         `,
@@ -43,7 +43,7 @@ export const careerPath = [
         description: `
         <p>
             <ul class="list-inside list-disc -indent-5 ml-6">
-                <li class="leading-6"> Senior Project: Eco-drive</li>
+                <li class="leading-8"> Senior Project: Eco-drive</li>
             </ul>
         </p>`,
     },
